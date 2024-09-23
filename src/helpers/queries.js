174e884323cde9f1513ad4fbca3ab05442d849d0
Augusto Brito <britoaugusto1@gmail.com>
2024-09-23 11:25:55 -0300

    agregamos una variable de entorno para guardar la direccion de json server

---
+++ src/helpers/queries.js
@@ -0,0 +1,12 @@
+const URLProducto= import.meta.env.VITE_API_PRODUCTO;
+//Peticiones o solicitudes
+
+//GET
+//PUT O PATH
+//DELETE
+
+//POST 
+export const crearProductoApi =()=>{
+console.log(URLProducto);
+
+}
