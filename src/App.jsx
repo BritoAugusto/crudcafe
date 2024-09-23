e84a99e86d43865c67b7810efc482d2d96649544
Augusto Brito <britoaugusto1@gmail.com>
2024-09-22 23:32:20 -0300

    se agrego el maquetado a los componentes

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,29 @@ import Menu from "./componentes/commons/Menu"
 import Footer from "./componentes/commons/Footer"
 import Inicio from "./componentes/pages/inicio"
 import Administrador from "./componentes/pages/Administrador"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import FormularioProducto from "./componentes/producto/FormularioProducto";
+import Error404 from "./componentes/pages/Erro404";
+import DetalleProducto from "./componentes/pages/DetalleProducto";
 
 function App() {
   return (
     <>
-      {/* <Menu></Menu> */}
-      {/* <Inicio></Inicio> */}
-      {/* <Administrador></Administrador> */}
-      {/* <Footer></Footer> */}
+       {/* <Menu></Menu>  */}
+       {/* <Inicio></Inicio> */}
+              {/* <Administrador></Administrador> */}
+       {/* <Footer></Footer>  */}
+       <BrowserRouter>
+       <Menu></Menu>
+       <Routes>
+        <Route exact path="/" element={<Inicio></Inicio>}></Route>
+       </Routes>
+       <Footer></Footer>
+       </BrowserRouter>
     
     </>
   )
--- src/componentes/commons/Footer.jsx
+++ src/componentes/commons/Footer.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
-            <p>detalles del footer</p>
-        </div>
+      <footer className="text-center bg-green text-light py-4">
+        <p>&copy; Todos los derechos reservados</p>
+      </footer>
     );
 };
 
