0f4a5044cbd2ec75a6930094d8c1371ba940aefb
Augusto Brito <britoaugusto1@gmail.com>
2024-09-24 22:59:35 -0300

    agregamos las rutas del administrador de forma protegida

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
 import Menu from "./componentes/commons/Menu";
 import Footer from "./componentes/commons/Footer";
 import Inicio from "./componentes/pages/inicio";
-import Administrador from "./componentes/pages/Administrador";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
-import "bootstrap-icons/font/bootstrap-icons.css";
-import FormularioProducto from "./componentes/producto/FormularioProducto";
 import Error404 from "./componentes/pages/Erro404";
 import DetalleProducto from "./componentes/pages/DetalleProducto";
 import Login from "./componentes/pages/Login";
-import { useState } from "react";
+import RutaProtegida from "./componentes/routes/RutaProtegida";
+import RutasAdmin from "./componentes/routes/RutasAdmin";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 function App() {
   const usuario = JSON.parse(sessionStorage.getItem("userKey")) || "";
@@ -32,33 +32,17 @@ function App() {
           ></Route>
           <Route
             exact
-            path="/administrador"
-            element={<Administrador></Administrador>}
-          ></Route>
-          <Route
-            exact
-            path="/administrador/crear"
-            element={
-              <FormularioProducto
-                titulo="Nuevo producto"
-                creandoProducto={true}
-              ></FormularioProducto>
-            }
-          ></Route>
-          <Route
-            exact
-            path="/administrador/editar/:id"
-            element={
-              <FormularioProducto
-                titulo="Editar producto"
-                creandoProducto={false}
-              ></FormularioProducto>
-            }
-          ></Route>
-          <Route
-            exact
             path="/login"
             element={<Login setUsuarioLogueado={setUsuarioLogueado}></Login>}
+          ></Route>
+          <Route
+            exact
+            path="/administrador/*"
+            element={
+              <RutaProtegida>
+                <RutasAdmin></RutasAdmin>
+              </RutaProtegida>
+            }
           ></Route>
           <Route path="*" element={<Error404></Error404>}></Route>
         </Routes>
--- src/componentes/routes/RutasAdmin.jsx
+++ src/componentes/routes/RutasAdmin.jsx
@@ -2,35 +2,35 @@ import Administrador from "../pages/Administrador";
 import FormularioProducto from "../producto/FormularioProducto";
 import { Route, Routes } from "react-router-dom";
 const RutasAdmin = () => {
-    return (
-      <Routes>
-        <Route
-          exact
-          path="/administrador"
-          element={<Administrador></Administrador>}
-        ></Route>
-        <Route
-          exact
-          path="/administrador/crear"
-          element={
-            <FormularioProducto
-              titulo="Nuevo producto"
-              creandoProducto={true}
-            ></FormularioProducto>
-          }
-        ></Route>
-        <Route
-          exact
-          path="/administrador/editar/:id"
-          element={
-            <FormularioProducto
-              titulo="Editar producto"
-              creandoProducto={false}
-            ></FormularioProducto>
-          }
-        ></Route>
-      </Routes>
-    );
+  return (
+    <Routes>
+      <Route
+        exact
+        path="/"
+        element={<Administrador></Administrador>}
+      ></Route>
+      <Route
+        exact
+        path="/crear"
+        element={
+          <FormularioProducto
+            titulo="Nuevo producto"
+            creandoProducto={true}
+          ></FormularioProducto>
+        }
+      ></Route>
+      <Route
+        exact
+        path="/editar/:id"
+        element={
+          <FormularioProducto
+            titulo="Editar producto"
+            creandoProducto={false}
+          ></FormularioProducto>
+        }
+      ></Route>
+    </Routes>
+  );
 };
 
 export default RutasAdmin;
