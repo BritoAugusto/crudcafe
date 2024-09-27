e4c8afff53552a1be33c492979fa9f9031df81df
Augusto Brito <britoaugusto1@gmail.com>
2024-09-27 20:33:53 -0300

    se agegó la logica en el maquetado de inicio para mostrar los productos creados desde la pagina administrador

--- src/componentes/pages/Inicio.jsx
+++ src/componentes/pages/Inicio.jsx
@@ -1,7 +1,30 @@
 import { Container, Row } from "react-bootstrap";
 import CardProducto from "../producto/CardProducto";
+import { useEffect, useState } from "react";
+import { leerProductoApi } from "../../helpers/queries";
+
 
 const Inicio = () => {
+const [productos, setProductos] = useState([]);
+
+ useEffect(() => {
+   obtenerProductos();
+ });
+
+const obtenerProductos = async () => {
+  const respuesta = await leerProductoApi();
+  if (respuesta.status === 200) {
+    const datos = await respuesta.json();
+    setProductos(datos);
+  } else {
+    Swal.fire({
+      title: "Ocurrio un error",
+      text: `En estos momentos no podemos mostrar la lista de productos, intentá nuevamente más tarde`,
+      icon: "error",
+    });
+  }
+};
+
   return (
     <section className="mainSection">
       <img
@@ -13,11 +36,9 @@ const Inicio = () => {
         <h1 className="display-4">Nuestros Productos</h1>
         <hr />
         <Row>
-          <CardProducto></CardProducto>
-          <CardProducto></CardProducto>
-          <CardProducto></CardProducto>
-          <CardProducto></CardProducto>
-          <CardProducto></CardProducto>
+          {productos.map((producto, index) => (
+            <CardProducto key={index} producto={producto}></CardProducto>
+          ))}
         </Row>
       </Container>
     </section>
